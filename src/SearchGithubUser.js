c6b9dc45b926efb7150317999297576a53e27bc3
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-07-17 16:03:53 +0800

    changes

---
+++ src/SearchGithubUser.js
@@ -0,0 +1,16 @@
+import './SearchGithubUser.css'
+
+export default function SearchGithubUser() {
+    return (
+        <div className='container'>
+            <div className='search-input'>
+                <form>
+
+                </form>
+            </div>
+            <div className='result'>
+
+            </div>
+        </div>
+    )
+}
