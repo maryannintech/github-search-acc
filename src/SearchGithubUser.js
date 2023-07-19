d4f918a8f9fead74229b7fa05914daab8f7feade
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-07-19 15:04:30 +0800

    fix the logic

--- src/SearchGithubUser.js
+++ src/SearchGithubUser.js
@@ -1,18 +1,51 @@
-import './SearchGithubUser.css'
+import "./SearchGithubUser.css";
+import { useState } from "react";
 
 export default function SearchGithubUser() {
-    return (
-        <div className='container'>
-            <div className='search-input'>
-                <form>
-                    <input type='text' placeholder='Search'></input>
-                    <button type='submit'><i class="bi bi-search"></i></button>
-                </form>
-            </div>
-            <div className='result'>
-                <div className='user-result'>
-                </div>
-            </div>
-        </div>
-    )
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const username = e.target.elements["user-search"].value;
+    const apiUrl = `https://api.github.com/users/${username}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      if (response) {
+        const data = await response.json();
+        setUserData(data);
+        setError(null);
+      } else {
+        setError("User not found");
+        setUserData(null);
+      }
+    } catch (err) {
+      setError("An error occurred while fetching data");
+      setUserData(null);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="search-input">
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Search" id="user-search"></input>
+          <button type="submit">
+            <i className="bi bi-search"></i>
+          </button>
+        </form>
+      </div>
+      <div className="result">
+        <img src={userData.avatar_url} alt="avatar of user"></img>
+        {error && <div className="error">{error}</div>}
+        {userData && (
+          <div className="user-info">
+            <p>Username: {userData.login}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
